c9339b0dd77a14240ed49e68cf203e51da265680
teispec <teispec0911@gmail.com>
2013-01-14 12:14:29 +0900

    アメーバピグapk

--- src/com/example/usi/MainActivity.java
+++ src/com/example/usi/MainActivity.java
@@ -2,21 +2,47 @@ package com.example.usi;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private WebView webview;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		webview = (WebView) findViewById(R.id.webView1);
+		webview.setWebViewClient(new WebViewClient()); // リンクをタップしたときに標準ブラウザを起動させない
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setPluginsEnabled(true);
+		webview.getSettings()
+				.setUserAgentString(
+						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7");
+
+		webview.loadUrl("http://pigg.ameba.jp/core/main");
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (event.getAction() == KeyEvent.ACTION_DOWN
+		&& keyCode == KeyEvent.KEYCODE_BACK
+		&& webview.canGoBack() == true) {
+			webview.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
